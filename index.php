163efc0fca798ef7cac4827482c82ab3e043e6e9
apgit33 <adrienpaturot@yahoo.fr>
2020-06-23 09:00:55 +0200

    Home page

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+include 'header.php';
+
+
+echo "
+<section class='intro'> 
+    <header>
+        <h1></h1>
+    </header>
+    <p class='text-intro'></p>
+
+    <ul>
+
+    </ul>
+</section>
+";
+
+include 'footer.php';
+
+
+
