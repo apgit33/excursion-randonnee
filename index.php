277919ec90418c4e257acbc8a752d406d31e3c6b
apgit33 <adrienpaturot@yahoo.fr>
2020-06-28 11:37:57 +0200

    Login form

--- index.php
+++ index.php
@@ -1,100 +1,58 @@
 <?php
 session_start();
 require_once 'bdd.php';
-require_once 'function.php';
 
-$user = (isset($_POST['user']))?$_POST['user']:'';
-$pwd = (isset($_POST['password']))?$_POST['password']:'';
 
 if(isset($_GET['action'])=='logout') {
     header('Location: index.php');
-    $_SESSION['user'] = '';
+    $_SESSION['user_firstname'] = '';
+    $_SESSION['user_lastname'] = '';
+    $_SESSION['login'] = false;
     $_SESSION['admin'] = false;
+    $_SESSION['id'] = '';
     exit;
 }
-
-
-if ($user !='' && $pwd != '') {
-    $query = "SELECT * FROM `nc_admin` WHERE a_user = ? AND password = ?";
-
-    		
-    $reponse = doIt2($query,$user,$pwd);
-
-    while ($donnees = $reponse->fetch()) {
-        $_SESSION['user'] = $donnees['a_user'];
-        $_SESSION['admin'] = true;
-    }
+//Si l'utilisateur est déjà connecté, on l'envoie sur son profil
+if(isset($_SESSION['login']) && $_SESSION['login']===true){ 
+    header('Location: dashboard.php');
+    exit;
 }
-
-
-
 include 'header.php';
 
 ?>
 
-<?php if(isset($_SESSION['admin']) && $_SESSION['admin']===true){
-?>
-    <div class='column is-5'>
-        <div class='card'>
-            <div class='card-image'>
-                <figure class='image is-4by3'>
-                    <img src='https://bulma.io/images/placeholders/1280x960.png' alt='Placeholder image'>
-                </figure>
-            </div>
-            <div class='card-content'>
-                <div class='media'>
-                    <div class='media-left'>
-                        <figure class='image is-48x48'>
-                            <img src='https://bulma.io/images/placeholders/96x96.png' alt='Placeholder image'>
-                        </figure>
-                    </div>
-                    <div class='media-content'>
-                        <p class='title is-4'><?=$_SESSION['user']?></p>
-                    </div>
-                </div>
-
-                <div class='content'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Phasellus nec iaculis mauris.
-                </div>
-            </div>
-        </div>
-    </div>
-<?php
-}else{
-    ?>
     <div class='columns'>
         <div class='column'>
             <h2 class="title is-2 is-centered">Connection</h2>
             <form action='' method='POST' id='form-login'>
-                
                 <div class='field'>
-                    <label for='user'>Login :</label>
+                    <label for='email'>Login :</label>
                     <div class='control'>
-                        <input class='input' type='text' name='user'  placeholder='Enter your user name' id='user' required >
+                        <input class='input' type='email' name='email'  placeholder='Enter your email' id='email'  >
                     </div>
                 </div>
-
                 <div class='field'>
                     <label for='password'>Password :</label>
                     <div class='control'>
-                        <input class="input" type='password' name='password' placeholder='Enter your password' id='password' required>
+                        <input class="input" type='password' name='password' placeholder='Enter your password' id='password' >
                     </div>
                 </div>
-
                 <div class='field'>
                     <div class='control'>
-                        <input type='submit' class='input is-primary button' value='Connection'>
+                        <input type='submit' class='input is-success button' value='Connection'>
                     </div>
                 </div>
             </form>
+            <ul id='verif'></ul>
         </div>
     </div>
-<?php
-}
+                 </div>       
+        </main>
+        <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
+        <script src='./src/login.js'></script>
+    </body>
+</html>
 
-
-include 'footer.php';
 
 
 
